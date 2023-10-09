237d06e24e207a5a966b5e040b8ea50d7f0b1647
Ruiwen523 <ruiwen523@gmail.com>
2023-10-09 17:47:22 +0800

    add Swagger套件

---
+++ MyBlog/MyBlog/Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+
+namespace MyBlog
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // DI Container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Document",
+                    Version = "v1",
+                    Description = "An ASP.NET Core Web API for managing My Blog",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "API Document", Version = "v2" });
+            });
+
+            //services.AddSwaggerGenNewtonsoftSupport(); // Swashbuckle.AspNetCore.Newtonsoft
+        }
+
+        // Middleware
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "/api/swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("v2/swagger.json", "My API V2");
+                c.RoutePrefix = "api/swagger";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                //endpoints.MapSwagger();
+            });
+        }
+    }
+}
---
+++ MyBlog/MyBlog/readme.txt
@@ -0,0 +1,23 @@
+﻿
+
+
+
+Use Ctrl+T quick search file and then use Alt+\ search membert method
+ex: 搜尋"Starup.cs"然後輸入"ConfigureServices"
+
+1.安裝EFCore SqlServer & Tools
+
+2.安裝Swagger套件
+ref: 
+ a. https://github.com/domaindrivendev/Swashbuckle.AspNetCore
+ b. https://dotblogs.com.tw/xinyikao/2021/07/04/183326
+ c. https://dotblogs.com.tw/yc421206/2022/03/12/via_swashbuckle_write_swagger_doc_in_asp_net_core_web_api
+
+T: Startup.cs
+M: ConfigureServices()
+	add: services.AddSwaggerGen(); 以及定義 c.SwaggerDoc();swagger文件
+	add: 安裝Swashbuckle.AspNetCore.Newtonsoft //services.AddSwaggerGenNewtonsoftSupport(); // 
+
+啟用瀏覽器並查看預設使用以下url 
+/swagger/{documentName}/swagger.json => "v1"
+/swagger/
